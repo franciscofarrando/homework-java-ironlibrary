3dc666eaaac06a21613d056160a7b96ed79e0257
Javier Moneo <jvr.moneo@gmail.com>
2025-04-22 15:48:30 +0200

    implement book

--- src/main/java/com/example/ironlibrary/models/Book.java
+++ src/main/java/com/example/ironlibrary/models/Book.java
@@ -1,4 +1,60 @@
 package com.example.ironlibrary.models;
 
 public class Book {
+    private String isbn;
+    private String title;
+    private String category;
+    private int quantity;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, String category, int quantity) {
+        this.isbn = isbn;
+        this.title = title;
+        this.category = category;
+        this.quantity = quantity;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
