3017fc818d1626ad8461590c5e6d8b4a86089370
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-22 15:43:32 +0200

    create class and write

--- src/main/java/com/example/ironlibrary/models/Student.java
+++ src/main/java/com/example/ironlibrary/models/Student.java
@@ -1,4 +1,36 @@
 package com.example.ironlibrary.models;
 
 public class Student {
+
+    private String usn;
+    private String name;
+    public Student() {}
+    public Student(String usn, String name) {
+        this.usn = usn;
+        this.name = name;
+    }
+
+    public String getUsn() {
+        return usn;
+    }
+
+    public void setUsn(String usn) {
+        this.usn = usn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "usn='" + usn + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
