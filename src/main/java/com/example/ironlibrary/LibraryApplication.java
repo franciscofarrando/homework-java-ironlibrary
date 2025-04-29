20e0b33c2545ebe88c04661e47a0eec7f0794833
Javier Moneo <jvr.moneo@gmail.com>
2025-04-29 10:06:37 +0200

    LibraryApplication

---
+++ src/main/java/com/example/ironlibrary/LibraryApplication.java
@@ -0,0 +1,25 @@
+package com.example.ironlibrary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.Library.Library")
+public class LibraryApplication implements CommandLineRunner {
+
+    @Autowired
+    private AppHandler appHandler;
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        appHandler.menu();
+    }
+}
